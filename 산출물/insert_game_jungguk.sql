26908f2e79321d3fa100d5ce79607be00c535a41
itwill <itwill@BOOK-SL8CRNQ5BJ>
2023-03-28 18:46:56 +0900

    ..

---
+++ 산출물/insert_game_jungguk.sql
@@ -0,0 +1,6 @@
+/**********************cart insert************************/
+insert into cart(c_no,u_no,g_no) values (cart_cart_no_SEQ.nextval, 'user1', 1);
+insert into cart(c_no,u_no,g_no) values (cart_cart_no_SEQ.nextval, 'user1', 2);
+insert into cart(c_no,u_no,g_no) values (cart_cart_no_SEQ.nextval, 'user1', 3);
+insert into cart(c_no,u_no,g_no) values (cart_cart_no_SEQ.nextval, 'user2', 1);
+insert into cart(c_no,u_no,g_no) values (cart_cart_no_SEQ.nextval, 'user2', 4);
