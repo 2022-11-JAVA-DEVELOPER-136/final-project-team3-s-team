08d1abc298ab1a3b037fb8d4c7b4bd5fe50130dc
rumi <rumi@192.168.1.55>
2023-03-29 17:50:57 +0900

    ..

--- src/main/java/com/itwill/steam/FinalProjectTeam3STeamApplication.java
+++ src/main/java/com/itwill/steam/FinalProjectTeam3STeamApplication.java
@@ -1,9 +1,18 @@
 package com.itwill.steam;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = {"com.itwill.steam.card.mapper",
+							"com.itwill.steam.cart.mapper",
+							"com.itwill.steam.category.mapper",
+							"com.itwill.steam.chat.mapper",
+							"com.itwill.steam.user.mapper",
+							"com.itwill.steam.game.mapper",
+							"com.itwill.steam.order.mapper",
+							"com.itwill.steam.review.mapper"})
 public class FinalProjectTeam3STeamApplication {
 
 	public static void main(String[] args) {
