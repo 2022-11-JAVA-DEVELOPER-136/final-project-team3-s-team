ad9b3a1c1efbaa073b43aa5dda20ec3b90963933
hiimmurloc <ac1324ac2@gmail.com>
2023-04-03 02:44:31 +0900

    오류 나지 않도록 implement만 하였음.

--- src/main/java/com/itwill/steam/order/YS/OrderDaoImplYS.java
+++ src/main/java/com/itwill/steam/order/YS/OrderDaoImplYS.java
@@ -1,5 +1,45 @@
 package com.itwill.steam.order.YS;
 
+import java.util.List;
+
+import com.itwill.steam.order.Order;
+
 public class OrderDaoImplYS implements OrderDaoYS {
+
+	@Override
+	public int insertOrder(int oNo) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<Order> findByUserNo(int uNo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Order> findByOrderNo(int oNo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int deleteByUserNo(int uNo) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int deleteByOrderNo(int oNO) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<Order> findByUserGame(int uNo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
