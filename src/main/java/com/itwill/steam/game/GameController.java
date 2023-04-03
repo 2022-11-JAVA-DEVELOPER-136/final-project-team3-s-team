2844ff41febcdcd23f075a539f58fcdfcd4f7f94
hiimmurloc <ac1324ac2@gmail.com>
2023-04-03 17:49:19 +0900

    thymeleaf 설정 및 일부 껍데기파일 생성. index.html(웰컴파일) 생성.

--- src/main/java/com/itwill/steam/game/GameController.java
+++ src/main/java/com/itwill/steam/game/GameController.java
@@ -1,5 +1,29 @@
 package com.itwill.steam.game;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class GameController {
 
+	@Autowired
+	private GameService gameService;
+	
+	public GameController() {
+		
+	}
+	
+	@RequestMapping("/steam_main")
+	public String steam_main() {
+		return "steam_main";
+	}
+	
+	
+	
+	@ExceptionHandler(Exception.class)
+	public String exception_handler(Exception e) {
+		return "steam_error";
+	}
 }
