b193771b5011ad82c65d087b79442cfffe6eabb5
hiimmurloc <ac1324ac2@gmail.com>
2023-04-06 09:26:47 +0900

    ..

--- src/main/java/com/itwill/steam/game/GameController.java
+++ src/main/java/com/itwill/steam/game/GameController.java
@@ -1,7 +1,10 @@
 package com.itwill.steam.game;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,37 +21,51 @@ public class GameController {
 	
 	//메인화면
 	@RequestMapping("/steam_main")
-	public String steam_main() {
+	public String steamMain() {
 		return "steam_main";
 	}
 	
 	//상품리스트 화면
-	@RequestMapping("/steam_list")
-	public String steam_list() {
-		return "steam_list";
+	@RequestMapping("/game_list")
+	public String gameList(Model model) {
+		
+		List<Game> gameList = gameService.findAllGames();
+		model.addAttribute("gameList", gameList);
+		
+		return "game_list";
+	}
+	
+	//상품 상세보기
+	@RequestMapping("/game_detail")
+	public String gameDetail(Model model) {
+		
+		Game game = gameService.findGameByNo(1);
+		model.addAttribute("game", game);
+		
+		return "game_detail";
 	}
 	
 	//상품리스트에서 제목 검색
-	@RequestMapping("/steam_search")
-	public String steam_search() {
-		return "steam_search";
+	@RequestMapping("/game_search")
+	public String gameSearch() {
+		return "game_search";
 	}
 	
 	//상품리스트에서 필터링 (카테고리, 태그, 언어)
-	@RequestMapping("/steam_filtering")
-	public String steam_filtering() {
-		return "steam_filtering";
+	@RequestMapping("/game_filtering")
+	public String gameFiltering() {
+		return "game_filtering";
 	}
 	
 	//POST방식으로 요청해야 하는 method에 GET방식으로 요청하는 경우, steam_main으로 redirect
 	@GetMapping({})
-	public String game_get() {
+	public String gameGet() {
 		return "redirect:steam_main";
 	}
 	
 	//Local Exception Handler
 	@ExceptionHandler(Exception.class)
-	public String exception_handler(Exception e) {
+	public String localExceptionHandler(Exception e) {
 		return "steam_error";
 	}
 }
