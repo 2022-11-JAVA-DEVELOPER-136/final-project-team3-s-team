78cefecc7a727973a013a21875c1b230eef4af16
hiimmurloc <ac1324ac2@gmail.com>
2023-04-07 16:12:56 +0900

    템플릿 추가

--- src/main/java/com/itwill/steam/game/GameController.java
+++ src/main/java/com/itwill/steam/game/GameController.java
@@ -22,7 +22,7 @@ public class GameController {
 	//root
 	@GetMapping("/")
 	public String welcome() {
-		return "redirect:steam_main";
+		return "redirect:main";
 	}
 	
 	//메인화면
@@ -74,4 +74,85 @@ public class GameController {
 	public String localExceptionHandler(Exception e) {
 		return "steam_error";
 	}
+	
+	
+	
+	/***************************** template test start *****************************/
+	@RequestMapping("/404")
+	public String fileNotFoundError() {
+		return "404";
+	}
+	@RequestMapping("/about")
+	public String about() {
+		return "about";
+	}
+	@RequestMapping("/checkout-address")
+	public String checkoutAddress() {
+		return "checkout-address";
+	}
+	@RequestMapping("/checkout-order")
+	public String checkoutOrder() {
+		return "checkout-order";
+	}
+	@RequestMapping("/checkout-payment")
+	public String checkoutPayment() {
+		return "checkout-payment";
+	}
+	@RequestMapping("/coming_soon")
+	public String comingSoon() {
+		return "coming_soon";
+	}
+	@RequestMapping("/contact")
+	public String contact() {
+		return "contact";
+	}
+	@RequestMapping("/forum")
+	public String forum() {
+		return "forum";
+	}
+	@RequestMapping("/forum-topic")
+	public String forumTopic() {
+		return "forum-topic";
+	}
+	@RequestMapping("/help-center")
+	public String helpCenter() {
+		return "help-center";
+	}
+	@RequestMapping("/help-center-article")
+	public String helpCenterArticle() {
+		return "help-center-article";
+	}
+	@RequestMapping("/main")
+	public String main() {
+		return "main";
+	}
+	@RequestMapping("/news")
+	public String news() {
+		return "news";
+	}
+	@RequestMapping("/news-article")
+	public String newsArticle() {
+		return "news-article";
+	}
+	@RequestMapping("/one-page")
+	public String onePage() {
+		return "one-page";
+	}
+	@RequestMapping("/private-messaging")
+	public String privateMessaging() {
+		return "private-messaging";
+	}
+	@RequestMapping("/profile")
+	public String profile() {
+		return "profile";
+	}
+	@RequestMapping("/store")
+	public String store() {
+		return "store";
+	}
+	@RequestMapping("/store-product")
+	public String storeProduct() {
+		return "store-product";
+	}
+	/****************************** template test end ******************************/
 }
