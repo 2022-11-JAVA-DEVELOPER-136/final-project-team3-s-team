ad94a65d55d1940b3e3319f7d49af009a9175246
itwill <itwill@BOOK-SL8CRNQ5BJ>
2023-04-07 17:21:38 +0900

    ..

--- src/main/java/com/itwill/steam/cart/CartController.java
+++ src/main/java/com/itwill/steam/cart/CartController.java
@@ -1,5 +1,18 @@
 package com.itwill.steam.cart;
 
-public class CartController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
+@Controller
+public class CartController {
+	
+	@Autowired
+	private CartService cartService;
+	
+	public CartController() {
+		
+	}
+	
+	
+	
 }
