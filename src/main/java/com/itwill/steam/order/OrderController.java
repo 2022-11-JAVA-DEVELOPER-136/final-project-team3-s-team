fa43ca363644dbe77dd7741aebfc85dab019893a
vjvjd <vjvjd@WIN-6GM4E2GQTUN>
2023-04-13 12:39:11 +0900

    ..

--- src/main/java/com/itwill/steam/order/OrderController.java
+++ src/main/java/com/itwill/steam/order/OrderController.java
@@ -1,5 +1,20 @@
 package com.itwill.steam.order;
 
-public class OrderController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
+@Controller
+public class OrderController {
+	@Autowired
+	private OrderService orderService;
+	
+	public OrderController() {
+		
+	}
+	
+	public String welcome() {
+		return "redirect:steam_main";
+	}
+	
+	
 }
