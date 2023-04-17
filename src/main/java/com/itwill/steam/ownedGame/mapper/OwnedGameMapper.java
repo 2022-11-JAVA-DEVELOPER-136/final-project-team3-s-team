9552deb3ad6fec5b07217d7ada1d53ffcfd28565
alterack <kimjun030519@gmail.com>
2023-04-17 17:51:28 +0900

    ..

--- src/main/java/com/itwill/steam/ownedGame/mapper/OwnedGameMapper.java
+++ src/main/java/com/itwill/steam/ownedGame/mapper/OwnedGameMapper.java
@@ -1,5 +1,9 @@
 package com.itwill.steam.ownedGame.mapper;
 
-public interface OwnedGameMapper {
+import java.util.List;
 
+import com.itwill.steam.ownedGame.OwnedGame;
+
+public interface OwnedGameMapper {
+	public List<OwnedGame> ownedGameList(int uNo);
 }
