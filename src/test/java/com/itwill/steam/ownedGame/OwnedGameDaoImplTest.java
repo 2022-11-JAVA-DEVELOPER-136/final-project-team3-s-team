5e3d112f80635990c023b6eb4bac0924c02a47a2
alterack <kimjun030519@gmail.com>
2023-04-18 13:22:46 +0900

    ..

--- src/test/java/com/itwill/steam/ownedGame/OwnedGameDaoImplTest.java
+++ src/test/java/com/itwill/steam/ownedGame/OwnedGameDaoImplTest.java
@@ -1,14 +1,23 @@
 package com.itwill.steam.ownedGame;
-
+	
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
-
+import org.springframework.beans.factory.annotation.Autowired;
+	
 class OwnedGameDaoImplTest {
-
+	
+	@Autowired
+	private OwnedGameDao ownedGameDao;
+	
 	@Test
 	void testOwnedGameList() {
-		fail("Not yet implemented");
+		List<OwnedGame> ownedGameList = ownedGameDao.ownedGameList(1);
+		assertNotNull(ownedGameList);
+		System.out.println(">>>>> " + ownedGameList);
+		
 	}
-
-}
+	
+}	
