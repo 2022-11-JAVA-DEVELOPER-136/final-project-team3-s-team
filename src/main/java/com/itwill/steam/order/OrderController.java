cd912a0df658ec203db95f333ce4af9b5ae4c380
vjvjd <vjvjd@192.168.219.103>
2023-04-20 18:48:02 +0900

    .

--- src/main/java/com/itwill/steam/order/OrderController.java
+++ src/main/java/com/itwill/steam/order/OrderController.java
@@ -8,12 +8,15 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.itwill.steam.card.Card;
 import com.itwill.steam.card.CardService;
+import com.itwill.steam.cart.Cart;
+import com.itwill.steam.cart.CartService;
 import com.itwill.steam.user.LoginCheck;
 import com.itwill.steam.user.User;
 import com.itwill.steam.user.UserService;
@@ -26,6 +29,8 @@ public class OrderController {
 	private CardService cardService;
 	@Autowired
 	private UserService userService;
+	@Autowired
+	private CartService cartService;
 	
 	public OrderController() {
 		
@@ -40,16 +45,64 @@ public class OrderController {
 		request.setAttribute("loginUser", loginUser);
 		return "checkout-address";
 	}
-	//카드 불러오기
-	
+	//카드정보 불러오기
 	@RequestMapping(value = "/checkout-payment")
 	public String checkoutCard(HttpServletRequest request) throws Exception {
 		User loginUser =(User)request.getSession().getAttribute("loginUser");
-		
+		if(loginUser==null) {
+			return "redirect:main";
+		}
 		Card card=cardService.findCardByNo(loginUser.getUNo());
+		String[] exDateArray =  card.getCardExDate().split("/");//달,년도 분리
+		String month = exDateArray[0];
+		String year = exDateArray[1];
+		
 		request.setAttribute("card", card);
-		System.out.println(">>>>>>>>>>>>>>>>>>>>>>"+card);
+		request.setAttribute("month", month);//카드 날짜 불러오기
+		request.setAttribute("year", year);//카드 년도 불러오기
+		
+		//card summery 
+		List<Cart> orderItem = cartService.selectCart(loginUser.getUNo());
+		request.setAttribute("orderItem", orderItem);
+		
+		  // 총액 계산
+        int fullPrice = 0;
+        int discountPrice = 0;
+        int savedPrice = 0;
+        int finalPrice = 0;
+        for (Cart cart : orderItem) {
+            fullPrice += cart.getGame().getGPrice();
+            discountPrice=(int) (fullPrice*cart.getGame().getGDiscountRate()/100);
+        }
+        savedPrice=fullPrice-discountPrice;
+        finalPrice=fullPrice-discountPrice;
+        request.setAttribute("fullPrice", fullPrice);
+        request.setAttribute("discountPrice", discountPrice);
+        request.setAttribute("savedPrice", savedPrice);
+        request.setAttribute("finalPrice", finalPrice);
+		
 		return "checkout-payment";
 	}
+
+
+	
+	//주문생성
+	@RequestMapping("/order_insert_action")
+	public String order_insert_action(Order order,Model model,HttpSession session) {
+		String forwardPath = "";
+		User loginUser=(User) session.getAttribute("loginUser");
+		try {
+			orderService.insertOrder(order);
+			model.addAttribute("successMessage","주문이 되었습니다.");
+			model.addAttribute("order",order);
+			forwardPath = "main";
+		}catch (Exception e) {
+			e.printStackTrace();
+			model.addAttribute("errorMessage", "주문이 실패하였습니다.");
+			forwardPath = "error";
+		}
+		return forwardPath;
+	};
+	
 	//라이브러리로 정보 옮기기
 }
--- src/main/resources/static/js/order.js
+++ src/main/resources/static/js/order.js
@@ -1,3 +1,28 @@
 /**
 order.js
 */
+$(function(){
+	//checkout-address페이지의 Billing Details 뜨게 하는 이벤트 등록
+	$('#billing_address_1').on('keyup', function(){
+		$('#billing_details_street').html($('#billing_address_1').val());
+	});
+	$('#billing_city').on('keyup', function(){
+		$('#billing_details_city').html($('#billing_city').val());
+	});
+	$('#state').on('click', function(){
+		val = $('#billing_details_stateFull option:selected').text($('#state').text());
+	});
+	$('#billing_postcode').on('keyup', function(){
+		$('#billing_details_Zip').html($('#billing_postcode').val());
+	});
+	$('#billing_phone').on('keyup', function(){
+		$('#billing_details_phoneNumber').html($('#billing_phone').val());
+	});
+	
+	//주문완료시 alert 띄우기
+	$('#orderBt').on('click', function(){
+		alert('주문완료~');
+	});
+	
+	console.log($('#state option'));
+});
