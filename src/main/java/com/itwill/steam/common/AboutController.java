7585e6de7964600a15ddf8e12d69d0dee062b152
hiimmurloc <ac1324ac2@gmail.com>
2023-04-27 10:18:59 +0900

    about 페이지에서 common 파일 정상작동하게 컨트롤러 수정

--- src/main/java/com/itwill/steam/common/AboutController.java
+++ src/main/java/com/itwill/steam/common/AboutController.java
@@ -9,9 +9,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.itwill.steam.cart.Cart;
+import com.itwill.steam.cart.CartService;
+import com.itwill.steam.category.Category;
 import com.itwill.steam.game.Game;
 import com.itwill.steam.game.GameService;
 import com.itwill.steam.game.SearchDto;
+import com.itwill.steam.user.User;
 
 import lombok.RequiredArgsConstructor;
 
@@ -20,6 +24,7 @@ import lombok.RequiredArgsConstructor;
 public class AboutController {
 	
 	private final GameService gameService;
+	private final CartService cartService;
 	
 	@RequestMapping("/about")
 	public String about(Model model, HttpSession session) {
@@ -28,6 +33,20 @@ public class AboutController {
 		
 		List<Game> popularGameList = gameService.findGames(searchDto);
 		model.addAttribute("popularGameList", popularGameList);
+		
+		/*****common-navbar.html에서 사용*****/
+        List<Category> categoryList = gameService.findAllCategory();
+		model.addAttribute("categoryList", categoryList);
+		
+		User loginUser = (User)session.getAttribute("loginUser");
+		if(loginUser!=null) {
+			int cartQuantity = cartService.countCart(loginUser.getUNo());
+			model.addAttribute("cartQuantity", cartQuantity);
+			
+			List<Cart> cartList = cartService.selectCart(loginUser.getUNo());
+	        model.addAttribute("cartList", cartList);
+		}
+		/*************************************/
 		
 		return "about";
 	}
